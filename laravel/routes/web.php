9109daec3ba2bb2f9bc8bdda8e5d910d85ad50f1
aachan1127 <aachan1127y@gmail.com>
2024-06-30 15:20:52 +0900

    質問追加

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ChatController::class, 'index']);
+Route::get('/chat/{message}', [ChatController::class, 'chat']);
 Route::post('/passer', [ChatController::class, 'passer']);
